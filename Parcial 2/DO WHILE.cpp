761e6ae8ebaa6cc2fb44ef9a7e586990097f2a0e
Rfl1 <rafael.salazar360@gmail.com>
2024-10-29 13:01:54 -0700

    Add files via upload

---
+++ Parcial 2/DO WHILE.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<stdlib.h>
+
+using namespace std;
+int main(){
+    
+    int num=20;
+    do{
+        cout<<num<<endl;
+        num--;
+    }
+    while(num>=1);
+    
+    system("pause");
+    return 0;
+    }
